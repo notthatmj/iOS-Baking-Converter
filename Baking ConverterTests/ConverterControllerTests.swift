93aa6b6798df2f54fe935b08d49718c982da8d5f
Michael Johnson <mj@notthatmj.com>
2017-06-21 17:40:24 -0500

    Factor out constants

--- Baking ConverterTests/ConverterControllerTests.swift
+++ Baking ConverterTests/ConverterControllerTests.swift
@@ -20,20 +20,20 @@ class FakeConverterScene: ConverterScene {
     }
 }
 
+let testIngredients: [Ingredient] = [ Ingredient(name: "All-Purpose Flour", gramsPerCup: 120.0),
+                                      Ingredient(name: "Cake Flour", gramsPerCup: 120.0),
+                                      Ingredient(name: "Sugar", gramsPerCup: 198.0),
+                                      Ingredient(name: "Soylent", gramsPerCup: 321.0)]
+
+let testInputUnitsOptions = [ VolumeUnit(name: "Cups", unitsPerCup: 1),
+                              VolumeUnit(name:"ml", unitsPerCup: 236.588) ]
+
+let testOutputUnitsOptions = [ MassUnit(name: "Grams", unitsPerGram: 1),
+                               MassUnit(name: "Ounces", unitsPerGram: 0.035274)]
+
 class ConverterController_DataSourceMethods_Tests: XCTestCase {
 
     var SUT: ConverterController!
-
-    let testIngredients: [Ingredient] = [ Ingredient(name: "All-Purpose Flour", gramsPerCup: 120.0),
-                                          Ingredient(name: "Cake Flour", gramsPerCup: 120.0),
-                                          Ingredient(name: "Sugar", gramsPerCup: 198.0),
-                                          Ingredient(name: "Soylent", gramsPerCup: 321.0)]
-    
-    let testInputUnitsOptions = [ VolumeUnit(name: "Cups", unitsPerCup: 1),
-                                  VolumeUnit(name:"ml", unitsPerCup: 236.588) ]
-    
-    let testOutputUnitsOptions = [ MassUnit(name: "Grams", unitsPerGram: 1),
-                                   MassUnit(name: "Ounces", unitsPerGram: 0.035274)]
     
     override func setUp() {
         
@@ -140,17 +140,6 @@ class ConverterController_DataSourceMethods_Tests: XCTestCase {
 class ConverterController_DataChangedMethods_Tests: XCTestCase {
     
     var SUT: ConverterController!
-    
-    let testIngredients: [Ingredient] = [ Ingredient(name: "All-Purpose Flour", gramsPerCup: 120.0),
-                                          Ingredient(name: "Cake Flour", gramsPerCup: 120.0),
-                                          Ingredient(name: "Sugar", gramsPerCup: 198.0),
-                                          Ingredient(name: "Soylent", gramsPerCup: 321.0)]
-    
-    let testInputUnitsOptions = [ VolumeUnit(name: "Cups", unitsPerCup: 1),
-                                  VolumeUnit(name:"ml", unitsPerCup: 236.588) ]
-    
-    let testOutputUnitsOptions = [ MassUnit(name: "Grams", unitsPerGram: 1),
-                                   MassUnit(name: "Ounces", unitsPerGram: 0.035274)]
     
     override func setUp() {
         
